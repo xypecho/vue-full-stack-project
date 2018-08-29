652c1af09aa78dc1bd5b5bd991428462da34803a
xypecho <xypecho@163.com>
2018-08-29 21:43:53 +0800

    数据库设计

--- fontend/src/pages/login/login.vue
+++ fontend/src/pages/login/login.vue
@@ -1,40 +1,40 @@
 <template>
-    <div class="login">
-        <div class="loginBox">
-            <el-tabs v-model="activeName" @tab-click="handleClick">
-                <el-tab-pane label="注册" name="signUp">
-                    <el-form :model="signUpForm" ref="signUpForm" label-width="100px" class="demo-dynamic" :rules="signUpFormRules">
-                        <el-form-item prop="username" label="用户名">
-                            <el-input v-model="signUpForm.username" size='small'></el-input>
-                        </el-form-item>
-                        <el-form-item prop="password" label="密码">
-                            <el-input type="password" v-model="signUpForm.password" size='small'></el-input>
-                        </el-form-item>
-                        <el-form-item prop="confirmPassword" label="确认密码">
-                            <el-input type="password" v-model="signUpForm.confirmPassword" size='small'></el-input>
-                        </el-form-item>
-                        <el-form-item>
-                            <el-button type="primary" size='small'>注册</el-button>
-                            <el-button size='small' @click="resetForm('signUpForm')">重置</el-button>
-                        </el-form-item>
-                    </el-form>
-                </el-tab-pane>
-                <el-tab-pane label="登录" name="signIn">
-                    <el-form :model="signInForm" ref="signInForm" label-width="100px" class="demo-dynamic" :rules="signInFormRules">
-                        <el-form-item prop="username" label="用户名">
-                            <el-input v-model="signInForm.username" size='small'></el-input>
-                        </el-form-item>
-                        <el-form-item prop="password" label="密码">
-                            <el-input type="password" v-model="signInForm.password" size='small'></el-input>
-                        </el-form-item>
-                        <el-form-item>
-                            <el-button type="primary" size='small'>登陆</el-button>
-                        </el-form-item>
-                    </el-form>
-                </el-tab-pane>
-            </el-tabs>
-        </div>
+  <div class="login">
+    <div class="loginBox">
+      <el-tabs v-model="activeName" @tab-click="handleClick">
+        <el-tab-pane label="注册" name="signUp">
+          <el-form :model="signUpForm" ref="signUpForm" label-width="100px" class="demo-dynamic" :rules="signUpFormRules">
+            <el-form-item prop="username" label="用户名">
+              <el-input v-model="signUpForm.username" size='small'></el-input>
+            </el-form-item>
+            <el-form-item prop="password" label="密码">
+              <el-input type="password" v-model="signUpForm.password" size='small'></el-input>
+            </el-form-item>
+            <el-form-item prop="confirmPassword" label="确认密码">
+              <el-input type="password" v-model="signUpForm.confirmPassword" size='small'></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" size='small' @click='register'>注册</el-button>
+              <el-button size='small' @click="resetForm('signUpForm')">重置</el-button>
+            </el-form-item>
+          </el-form>
+        </el-tab-pane>
+        <el-tab-pane label="登录" name="signIn">
+          <el-form :model="signInForm" ref="signInForm" label-width="100px" class="demo-dynamic" :rules="signInFormRules">
+            <el-form-item prop="username" label="用户名">
+              <el-input v-model="signInForm.username" size='small'></el-input>
+            </el-form-item>
+            <el-form-item prop="password" label="密码">
+              <el-input type="password" v-model="signInForm.password" size='small'></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" size='small'>登陆</el-button>
+            </el-form-item>
+          </el-form>
+        </el-tab-pane>
+      </el-tabs>
     </div>
+  </div>
 </template>
 <script>
 export default {
@@ -96,38 +96,43 @@ export default {
     },
     resetForm(formName) {
       this.$refs[formName].resetFields();
+    },
+    register() {
+      this.$axios.get('/get_banner').then(res => {
+        console.log(res);
+      });
     }
   }
 };
 </script>
 <style lang="stylus" scoped>
 .login {
-    background: rgba(0, 0, 0, 0.2);
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    position: relative;
-    width: 100%;
-    height: 100%;
+  background: rgba(0, 0, 0, 0.2);
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  position: relative;
+  width: 100%;
+  height: 100%;
 
-    &:before {
-        background-image: url('~@/assets/images/code.jpg');
-        background-size: cover;
-        content: '';
-        height: 100%;
-        left: 0;
-        position: absolute;
-        top: 0;
-        width: 100%;
-        z-index: -999;
-    }
+  &:before {
+    background-image: url('~@/assets/images/code.jpg');
+    background-size: cover;
+    content: '';
+    height: 100%;
+    left: 0;
+    position: absolute;
+    top: 0;
+    width: 100%;
+    z-index: -999;
+  }
 }
 
 .loginBox {
-    background-color: #fff;
-    margin-left: auto;
-    margin-right: 100px;
-    padding: 43px 43px 15px 43px;
-    font-size: 17px;
+  background-color: #fff;
+  margin-left: auto;
+  margin-right: 100px;
+  padding: 43px 43px 15px 43px;
+  font-size: 17px;
 }
 </style>
