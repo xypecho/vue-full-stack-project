9008ac0d3e81c015c736da0ef261f9de080ea25d
xypecho <xypecho@163.com>
2018-09-06 22:26:36 +0800

    数据库备份

--- backend/test.sql
+++ backend/test.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50711
 File Encoding         : 65001
 
-Date: 2018-09-05 16:32:35
+Date: 2018-09-06 22:26:21
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -21,7 +21,7 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `uid` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '用户id',
-  `utype` tinyint(1) unsigned DEFAULT NULL COMMENT '用户类型：0为管理员，1为普通用户',
+  `utype` tinyint(1) unsigned DEFAULT '1' COMMENT '用户类型：0为管理员，1为普通用户',
   `username` varchar(255) DEFAULT NULL COMMENT '用户名',
   `password` char(100) DEFAULT NULL COMMENT '密码',
   `email` varchar(255) DEFAULT NULL COMMENT '邮箱',
@@ -29,8 +29,9 @@ CREATE TABLE `users` (
   `status` tinyint(1) DEFAULT '1' COMMENT '状态：0为禁用，1为启用',
   `avatar` char(255) DEFAULT NULL COMMENT '头像',
   PRIMARY KEY (`uid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of users
 -- ----------------------------
+INSERT INTO `users` VALUES ('1', '0', 'admin', 'e10adc3949ba59abbe56e057f20f883e', 'xypecho@163.com', '1536236518370', '1', null);
