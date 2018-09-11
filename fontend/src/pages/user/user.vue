16ac00ae167d6b0ecc8a0b0a8df8f0348d112df5
xypecho <xypecho@163.com>
2018-09-11 17:07:26 +0800

    update

--- fontend/src/pages/user/user.vue
+++ fontend/src/pages/user/user.vue
@@ -2,28 +2,28 @@
  * @Author: xypecho
  * @Date: 2018-09-09 20:55:25
  * @Last Modified by: xueyp
- * @Last Modified time: 2018-09-11 12:16:22
+ * @Last Modified time: 2018-09-11 16:57:01
  */
 <template>
   <div class="user" v-loading='loading'>
     <el-table :data="userList" style="width: 100%;height:100%">
       <el-table-column fixed prop="username" label="用户名" width="150">
       </el-table-column>
-      <el-table-column prop="uid" label="id" width="120">
+      <el-table-column prop="uid" label="id">
       </el-table-column>
-      <el-table-column prop="utype" label="角色" width="120" :formatter="formatterUtype">
+      <el-table-column prop="utype" label="角色" :formatter="formatterUtype">
       </el-table-column>
-      <el-table-column prop="status" label="是否启用" width="120" :formatter="formatterStatus">
+      <el-table-column prop="status" label="是否启用" :formatter="formatterStatus">
       </el-table-column>
-      <el-table-column prop="is_deleted" label="帐号状态" width="120" :formatter="formatterIs_deleted">
+      <el-table-column prop="is_deleted" label="帐号状态" :formatter="formatterIs_deleted">
       </el-table-column>
-      <el-table-column prop="email" label="邮箱" width="120">
+      <el-table-column prop="email" label="邮箱">
       </el-table-column>
-      <el-table-column prop="register_time" label="注册时间" width="300" :formatter="formatterRegister_time">
+      <el-table-column prop="register_time" label="注册时间" :formatter="formatterRegister_time">
       </el-table-column>
-      <el-table-column prop="last_login_time" label="最后登录时间" width="120" :formatter="formatterLast_login_time">
+      <el-table-column prop="last_login_time" label="最后登录时间" :formatter="formatterLast_login_time">
       </el-table-column>
-      <el-table-column label="操作" width="120">
+      <el-table-column label="操作">
         <template slot-scope="scope">
           <el-button type="text" size="small">
             移除
