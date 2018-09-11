fb6927759b76586ba9f9eefd1833051ee3e8066c
xypecho <xypecho@163.com>
2018-09-11 09:50:31 +0800

    安装babel-polyfill包，解决ie报错bug

--- fontend/src/main.js
+++ fontend/src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import 'normalize.css';
+import 'babel-polyfill';
 import 'element-ui/lib/theme-chalk/index.css';
 import Vue from 'vue';
 import Vuex from 'vuex';
