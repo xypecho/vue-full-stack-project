d8ea176706836aab2d802df32ae19b60f7d2b964
xypecho <xypecho@163.com>
2018-10-30 19:56:31 +0800

    update

--- backend/common/mysqlDemo.js
+++ backend/common/mysqlDemo.js
@@ -1,10 +1,18 @@
+/*
+ * @Author: xypecho
+ * @Date: 2018-10-26 21:31:24
+ * @Last Modified by: xypecho
+ * @Last Modified time: 2018-10-30 19:55:52
+ */
 const mysql = require('mysql');
+const tool = require('./tool.js');
+
 
 db = mysql.createPool({
     host: 'localhost',
     post: '3306',
     user: 'root',
-    password: '123456',
+    password: tool.env() == 'production' ? 'bu-gao-su-ni' : '123456',
     database: 'test'
 });
 
