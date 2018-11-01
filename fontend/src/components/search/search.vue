b2243ec32eeaca7f2c22511108c029537479481b
xypecho <xypecho@163.com>
2018-11-01 22:14:19 +0800

    搜索组件更新

--- fontend/src/components/search/search.vue
+++ fontend/src/components/search/search.vue
@@ -6,10 +6,13 @@
  */
 <template>
   <div class="search">
-    <el-select v-model="value5" placeholder="请选择" size='mini'>
+    <el-select v-model="value5" placeholder="请选择帐号状态" size='mini' class="search-item">
       <el-option v-for="item in searchCondition" :key="item.value" :label="item.label" :value="item.value">
       </el-option>
     </el-select>
+    <el-date-picker v-model="value6" type="daterange" range-separator="至" start-placeholder="注册起始日期" end-placeholder="注册终止日期" size='mini' class="search-item search-item-data">
+    </el-date-picker>
+    <el-input v-model="input" placeholder="请输入用户名" class="search-item" size='mini'></el-input>
     <span class="search-btn earch-btn-search" @click='search'>搜索</span>
     <span class="search-btn earch-btn-reset" @click="reset">重置</span>
   </div>
@@ -18,13 +21,15 @@
 export default {
   data() {
     return {
-      value5: []
+      value5: [],
+      value6: ''
     };
   },
   props: {
     searchCondition: {
       type: Array,
-      defailt: []
+      defailt: [],
+      input: ''
     }
   },
   methods: {
@@ -38,13 +43,33 @@ export default {
 };
 </script>
 <style lang="stylus" scoped>
-.search
-  margin-bottom 20px
-  .search-btn
-    color #409EFF
-    font-size 14px
-    cursor pointer
-    margin 0 7px
-    &:last-child
-      margin-left 0px
+.search {
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+
+  .search-item {
+    margin: 0 10px;
+    flex: 0 0 200px;
+
+    &:first-child {
+      margin-left: 0;
+    }
+  }
+
+  .search-item-data {
+    flex: 0 0 290px;
+  }
+
+  .search-btn {
+    color: #409EFF;
+    font-size: 14px;
+    cursor: pointer;
+    margin: 0 10px;
+
+    &:last-child {
+      margin-left: 0px;
+    }
+  }
+}
 </style>
