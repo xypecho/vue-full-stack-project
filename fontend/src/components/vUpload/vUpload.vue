c7b31607f2a545f98f13d3f7920fc7bf1decf442
xypecho <xypecho@163.com>
2018-11-22 21:20:03 +0800

    写上传组件

---
+++ fontend/src/components/vUpload/vUpload.vue
@@ -0,0 +1,54 @@
+/*
+ * @Author: xypecho
+ * @Date: 2018-11-22 20:24:10
+ * @Last Modified by: xypecho
+ * @Last Modified time: 2018-11-22 21:17:12
+ */
+<template>
+    <div class="vUpload">
+        <div class="vUpload-progress"></div>
+        <div class="vUpload-button">
+            <el-button type="primary" size='mini' @click='uploadFile'>上传文件</el-button>
+            <input type="file" style="display:none" ref="uploadBtn">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    uploadFile() {
+      this.$alert(
+        '上传文件功能仅供学习交流之用，所上传的文件将在24小时之内自动删除。(请不要上传奇♂怪的东西哦→_→)',
+        '提示',
+        {
+          confirmButtonText: '确定',
+          callback: action => {
+            this.$message({
+              type: 'info',
+              message: `action: ${action}`
+            });
+            this.$refs.uploadBtn.click();
+          }
+        }
+      );
+    }
+  }
+};
+</script>
+<style lang='stylus' scoped>
+.vUpload {
+    margin-bottom: 20px;
+    display: flex;
+    align-items: center;
+
+    .vUpload-progress {
+        flex: 1;
+    }
+
+    .vUpload-button {
+        flex: 0 0 200px;
+        text-align: right;
+    }
+}
+</style>
--- fontend/src/pages/form/fileUpload.vue
+++ fontend/src/pages/form/fileUpload.vue
@@ -2,24 +2,96 @@
  * @Author: xypecho
  * @Date: 2018-11-21 21:45:27
  * @Last Modified by: xypecho
- * @Last Modified time: 2018-11-21 21:48:28
+ * @Last Modified time: 2018-11-22 20:24:03
  */
 <template>
     <div class="fileUpload">
-        <el-upload class="upload-demo" drag action="https://jsonplaceholder.typicode.com/posts/" multiple>
-            <i class="el-icon-upload"></i>
-            <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
-            <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
-        </el-upload>
+        <v-upload></v-upload>
+        <el-table :data="tableData" style="width: 100%">
+            <el-table-column fixed prop="date" label="日期" width="150">
+            </el-table-column>
+            <el-table-column prop="name" label="姓名" width="120">
+            </el-table-column>
+            <el-table-column prop="province" label="省份" width="120">
+            </el-table-column>
+            <el-table-column prop="city" label="市区" width="120">
+            </el-table-column>
+            <el-table-column prop="address" label="地址" width="300">
+            </el-table-column>
+            <el-table-column prop="zip" label="邮编" width="120">
+            </el-table-column>
+            <el-table-column fixed="right" label="操作" width="100">
+                <template slot-scope="scope">
+                    <el-dropdown>
+                        <span class="el-dropdown-link" style="cursor:pointer;color:#409eff">
+                            操作
+                        </span>
+                        <el-dropdown-menu slot="dropdown">
+                            <el-dropdown-item>查看</el-dropdown-item>
+                            <el-dropdown-item>删除</el-dropdown-item>
+                        </el-dropdown-menu>
+                    </el-dropdown>
+                </template>
+            </el-table-column>
+        </el-table>
     </div>
 </template>
 
 <script>
-export default {};
+import vUpload from '@/components/vUpload/vUpload';
+
+export default {
+  methods: {
+    handleClick(row) {
+      console.log(row);
+    }
+  },
+
+  data() {
+    return {
+      tableData: [
+        {
+          date: '2016-05-03',
+          name: '王小虎',
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1518 弄',
+          zip: 200333
+        },
+        {
+          date: '2016-05-02',
+          name: '王小虎',
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1518 弄',
+          zip: 200333
+        },
+        {
+          date: '2016-05-04',
+          name: '王小虎',
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1518 弄',
+          zip: 200333
+        },
+        {
+          date: '2016-05-01',
+          name: '王小虎',
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1518 弄',
+          zip: 200333
+        }
+      ]
+    };
+  },
+  components: {
+    vUpload
+  }
+};
 </script>
 <style lang='stylus' scoped>
-.fileUpload
-    padding: 40px 160px 60px 160px;
-    .upload-demo
-        text-align center
+.fileUpload {
+    padding: 20px;
+}
 </style>
