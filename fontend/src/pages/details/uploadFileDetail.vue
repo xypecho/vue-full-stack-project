baaad7e9f1d17e4f45c6f7891f3cc25dfccc69d5
xypecho <xypecho@163.com>
2018-12-11 22:45:49 +0800

    开始写瀑布流页面

--- fontend/src/pages/details/uploadFileDetail.vue
+++ fontend/src/pages/details/uploadFileDetail.vue
@@ -2,12 +2,29 @@
  * @Author: xypecho
  * @Date: 2018-12-10 19:58:29
  * @Last Modified by: xypecho
- * @Last Modified time: 2018-12-10 21:16:33
+ * @Last Modified time: 2018-12-11 21:32:48
  */
 <template>
   <div class="uploadFileDetail">
-    <h1>uploadFileDetail</h1>
-    <h1>{{ id }}</h1>
+    <div class="back_btn">
+      <el-button
+        type='primary'
+        size='mini'
+        @click='$router.back()'
+      >返回</el-button>
+    </div>
+    <div
+      class="waterfalls_wrapper"
+      ref='waterfalls_wrapper'
+    >
+      <div
+        class="waterfalls_item"
+        v-for="(item, index) in files"
+        :key="index"
+      >
+        <img :src="item">
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,21 +32,31 @@
 export default {
   data() {
     return {
-      id: this.$route.query.id
+      id: this.$route.query.id,
+      files: []
     };
   },
-  created() {
+  mounted() {
     this.getFileDetails();
   },
   methods: {
     getFileDetails() {
       this.$axios.post('/api/upload/getFileDetails', { id: this.id }).then(res => {
-        console.log('==========');
-        console.log(res.data.data[0]);
-        console.log('==========');
+        this.files = res.data.data[0].files.split(';');
+        this.$nextTick(() => {
+          const filesBox = this.$refs.waterfalls_wrapper.getElementsByClassName('waterfalls_item');
+          for (let i = 0; i < filesBox.length; i++) {
+            this.setElementCss(filesBox[i], this.files[i], i);
+          }
+        });
       }).catch(err => {
         console.error(err);
       });
+    },
+    setElementCss(element, file, index) {
+      console.log('============');
+      console.log(element, file, index);
+      console.log('============');
     }
   }
 };
@@ -44,5 +71,24 @@ export default {
   right: 0;
   bottom: 0;
   background: #fff;
+
+  .back_btn {
+    padding: 20px;
+    float: right;
+  }
+
+  .waterfalls_wrapper {
+    clear: both;
+    position: relative;
+
+    .waterfalls_item {
+      background: #fff;
+      position: absolute;
+      padding: 3px;
+      font-size: 0;
+      box-sizing: border-box;
+      transition: all 0.3s ease;
+    }
+  }
 }
 </style>
