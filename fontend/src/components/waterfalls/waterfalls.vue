dfea417dfb336256794c43eb30ce92b1763e0e1a
xypecho <xypecho@163.com>
2018-12-13 22:51:10 +0800

    封装瀑布流组件

---
+++ fontend/src/components/waterfalls/waterfalls.vue
@@ -0,0 +1,82 @@
+/*
+ * @Author: xypecho
+ * @Date: 2018-12-13 20:36:46
+ * @Last Modified by: xypecho
+ * @Last Modified time: 2018-12-13 22:40:04
+ */
+ <!--
+ @component name: 瀑布流组件
+ @param name: files 图片数组
+ @param name: fileWidth 单张图片的宽度
+ @param name: column 一行展示多少列
+ @param name: lazy 是否启用懒加载
+ -->
+<template>
+  <div
+    class="waterfalls"
+    ref="waterfalls"
+  >
+    <div
+      class="waterfalls-items"
+      v-for="(item,index) in files"
+      :key='index'
+    >
+      <img
+        :src="item"
+        alt=""
+        :width="fileWidth"
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    files: {
+      type: Array,
+      default: () => []
+    },
+    fileWidth: {
+      type: Number,
+      default: 200
+    },
+    column: {
+      type: Number,
+      default: 5
+    },
+    lazy: {
+      type: Boolean,
+      default: true
+    }
+  },
+  mounted() {
+    this.initWaterfalls();
+  },
+  methods: {
+    initWaterfalls() {
+      const item = this.$refs.waterfalls.getElementsByClassName('waterfalls-items');
+      for (let i = 0; i < item.length; i++) {
+        this.setWaterfallsStyle(item[i], this.files[i], i);
+      }
+    },
+    setWaterfallsStyle(element, file, index) {
+      element.style.left = `${index * (this.fileWidth + 20)}px`;
+    }
+  }
+};
+</script>
+<style lang='stylus' scoped>
+.waterfalls {
+  clear: both;
+  background: #fff;
+  width: 100%;
+  height: 100%;
+  position: relative;
+
+  .waterfalls-items {
+    position: absolute;
+    padding: 20px;
+  }
+}
+</style>
--- fontend/src/pages/details/uploadFileDetail.vue
+++ fontend/src/pages/details/uploadFileDetail.vue
@@ -2,7 +2,7 @@
  * @Author: xypecho
  * @Date: 2018-12-10 19:58:29
  * @Last Modified by: xypecho
- * @Last Modified time: 2018-12-12 22:08:24
+ * @Last Modified time: 2018-12-13 22:32:11
  */
 <template>
   <div
@@ -16,66 +16,68 @@
         @click='$router.back()'
       >返回</el-button>
     </div>
-    <div
-      class="waterfalls_wrapper"
-      ref='waterfalls_wrapper'
-    >
-      <div
-        class="waterfalls_item"
-        v-for="(item, index) in files"
-        :key="index"
-      >
-        <img :src="item">
-      </div>
-    </div>
+    <waterfalls
+      v-if="files && files.length>0"
+      :files='files'
+      :column='column'
+      :fileWidth='fileWidth'
+    ></waterfalls>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
+import waterfalls from '@/components/waterfalls/waterfalls';
 
 export default {
   data() {
     return {
       id: this.$route.query.id,
       files: [],
-      wrapperWidth: 0
+      column: 0,
+      fileWidth: 280
     };
   },
-  created() {
-    this.getFiles();
+  beforeMount() {
+    this.setWaterfallsStyle();
   },
   mounted() {
-    this.getFileDetails();
+    this.getFiles();
   },
   watch: {
     isCollapse() {
-      this.getFileDetails();
+      setTimeout(() => {
+        this.setWaterfallsStyle();
+      }, 500);
     }
   },
   methods: {
+    setWaterfallsStyle() {
+      this.$nextTick(() => {
+        const wrapperWidth = this.$refs.uploadFileDetail.offsetWidth;
+        this.column = Math.floor(wrapperWidth / (this.fileWidth + 20));
+      });
+    },
     getFiles() {
       this.$axios.post('/api/upload/getFileDetails', { id: this.id }).then(res => {
-        this.files = res.data.data[0].files.split(';');
+        if (res.data.status === 200) {
+          this.files = res.data.data[0].files.split(';');
+        } else {
+          this.$tips({
+            type: 'error',
+            message: '获取文件详情失败，请稍候重试'
+          });
+        }
       }).catch(err => {
         console.error(err);
       });
-    },
-    getFileDetails() {
-      this.wrapperWidth = document.querySelector('.uploadFileDetail').clientWidth;
-      this.$nextTick(() => {
-        const filesBox = this.$refs.waterfalls_wrapper.getElementsByClassName('waterfalls_item');
-        for (let i = 0; i < filesBox.length; i++) {
-          this.setElementStyle(filesBox[i], this.files[i], i);
-        }
-      });
-    },
-    setElementStyle(element, file, index) {
-      element.style.left = `${index * 150}px`;
     }
   },
   computed: {
     ...mapGetters(['isCollapse'])
+  },
+  components: {
+    waterfalls
   }
 };
 </script>
@@ -89,30 +91,11 @@ export default {
   right: 0;
   bottom: 0;
   background: #fff;
+  z-index: 100;
 
   .back_btn {
     padding: 20px;
     float: right;
-  }
-
-  .waterfalls_wrapper {
-    clear: both;
-    position: relative;
-    background: #fff;
-
-    .waterfalls_item {
-      position: absolute;
-      padding: 3px;
-      font-size: 0;
-      box-sizing: border-box;
-      transition: all 0.3s ease;
-      background: #fff;
-
-      & img {
-        width: 200px;
-        margin: 20px;
-      }
-    }
   }
 }
 </style>
