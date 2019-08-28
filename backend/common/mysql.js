2ceb939d1020ef0363c531c43f3e4f1d34473c4d
xypecho <xypecho@163.com>
2019-08-28 21:11:30 +0800

    配置nginx多项目

--- backend/common/mysql.js
+++ backend/common/mysql.js
@@ -2,7 +2,7 @@
  * @Author: xypecho
  * @Date: 2018-10-26 21:31:24
  * @Last Modified by: xueyp
- * @Last Modified time: 2018-11-20 16:17:34
+ * @Last Modified time: 2019-08-27 22:02:04
  */
 const mysql = require('mysql');
 const tool = require('./tool.js');
@@ -12,7 +12,7 @@ db = mysql.createPool({
     host: 'localhost',
     post: '3306',
     user: 'root',
-    password: tool.env() === 'production' ? 'bu-gao-su-ni' : '123456', // 'bu-gao-su-ni'是你线上数据库的密码，部署时记得修改
+    password: tool.env() === 'production' ? '123456' : '123456', // 'bu-gao-su-ni'是你线上数据库的密码，部署时记得修改
     database: 'test'
 });
 
